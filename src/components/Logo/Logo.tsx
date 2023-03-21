a4253d0f5c2715a5f8642fef0bb9491e77162cf2
Andrew <andrew.still@cbsinteractive.com>
2023-03-21 13:15:12 -0400

    Removed Logo Component

--- src/components/Logo/Logo.tsx
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import logo from "public/Paramount.svg"
-
-interface SVGRProps {
-    title?: string;
-}
-
-export const pLogo = (props: React.SVGProps<SVGSVGElement> & SVGRProps) => <img src={logo}/>;
