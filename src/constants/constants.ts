5ecb76f59153be02bb57903f2c4577be81e3b001
Andrew <andrew.still@cbsinteractive.com>
2023-04-18 11:15:05 -0400

    Added Dynamic Theme for Buttons

--- src/constants/constants.ts
+++ src/constants/constants.ts
@@ -1,7 +1,7 @@
 export type Sizes = 'large' | 'medium' | 'small';
 export type Styles = 'primary' | 'secondary' | 'tertiary';
 export type Width = 'hug' | 'full-width';
-export type Themes = 'light' | 'dark';
+export type Themes = 'light' | 'dark' | 'dynamic';
 export type Radius = '0' | '2' | '4' | '6' | '8' | '12' | 'full';
 export type States = 'default' | 'active' | 'error';
 export interface Item {
